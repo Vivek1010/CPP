65e723d13bf7610620dfa30611f7d5fcfbf73950
Vivek Sheel Banger <vivek.banger@gmail.com>
2023-10-19 04:29:29 +0000

    commiting Browser history another version

--- BrowserHistory.cpp
+++ BrowserHistory.cpp
@@ -3,9 +3,13 @@ int main()
 {
     string input[] = {"www.google.com", "www.amazon.com", "www.flipcart.com", "www.xyz.com"};
     BrowserHistory bh;
-    bh.visit(input[0]);
-    cout << bh.current() << endl;
-    bh.visit(input[1]);
+    for (string in : input)
+        bh.visit(in);
+
+    // bh.visit(input[0]);
+    // cout << bh.current() << endl;
+    // bh.visit(input[1]);
+
     cout << bh.current() << endl;
     bh.back();
     cout << bh.current() << endl;
